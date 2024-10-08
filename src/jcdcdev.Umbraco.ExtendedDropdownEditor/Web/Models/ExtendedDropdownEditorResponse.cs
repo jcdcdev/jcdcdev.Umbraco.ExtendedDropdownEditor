8a7a9fd51308b0a78ff44974d0f6cfa1cd8d17ad
jcdcdev <james@jcdc.dev>
2024-10-08 23:28:39 +0100

    feature Umbraco 15 support

--- src/jcdcdev.Umbraco.ExtendedDropdownEditor/Web/Models/ExtendedDropdownEditorResponse.cs
+++ src/jcdcdev.Umbraco.ExtendedDropdownEditor/Web/Models/ExtendedDropdownEditorResponse.cs
@@ -2,6 +2,6 @@
 
 public class ExtendedDropdownEditorResponse
 {
-    public List<string> Items { get; set; }
+    public List<string> Items { get; set; } = [];
     public Guid DataTypeKey { get; set; }
 }
